9c81ba4064e2631ade7465b919824431636b0f68
VladimirKoshovec <koshavladmir@gmail.com>
2022-11-15 14:04:16 +0200

    Task2_finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,17 @@ var throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
-let currentTime = JSON.parse(localStorage.getItem ('videoplayer-current-time'));
+const keyName = 'videoplayer-current-time';
 
-player.setCurrentTime (currentTime); 
+let currentTime = (key) => {
+    return localStorage.getItem(key) === null ? 0 : JSON.parse(localStorage.getItem(key));
+}
+
+player.setCurrentTime (currentTime(keyName)); 
 
 player.on ('timeupdate', throttle( onPlay, 1000));
 
 function onPlay (event) {
     let curSec = JSON.stringify(event.seconds);
-    localStorage.setItem ('videoplayer-current-time', curSec); 
+    localStorage.setItem (keyName, curSec); 
 }
