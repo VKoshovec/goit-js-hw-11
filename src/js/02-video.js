bd01079fc573dd0c44ee7db141e557bb6454bcfa
VladimirKoshovec <koshavladmir@gmail.com>
2022-11-15 10:19:14 +0200

    add_throtle_from_lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,14 @@
 import Vimeo from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Vimeo(iframe);
+let currentTime = JSON.parse(localStorage.getItem ('videoplayer-current-time'));
 
+player.setCurrentTime (currentTime); 
 
-console.log (iframe);
-console.log (player);
+player.on ('timeupdate', onPlay);
+
+function onPlay (event) {
+localStorage.setItem ('videoplayer-current-time', JSON.stringify(event.seconds)); 
+}
