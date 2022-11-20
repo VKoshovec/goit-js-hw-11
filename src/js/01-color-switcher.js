9540b6e3dae119ccf5e934abdc1aaee2a8b6e28c
VladimirKoshovec <koshavladmir@gmail.com>
2022-11-20 22:47:23 +0200

    Task1_start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+};
 
+refs.btnStart.addEventListener ('click', startColorChange);
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startColorChange () {
+    let inProgress = false;
+
+    colorTimer = setInterval (document.body.style.backgroundColor =  getRandomHexColor(), 1000);
+    inProgress = true;
+
+}
