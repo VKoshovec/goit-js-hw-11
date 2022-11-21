3d3babaaa8a628389691c0fe00b7fe2a3da261ca
VladimirKoshovec <koshavladmir@gmail.com>
2022-11-21 17:26:27 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,18 @@
-const { forEach } = require("lodash");
 
 const refs = {
    timerBox: document.querySelector ('.timer'),
    timerBoxCells: document.querySelectorAll('.field'),
+   timerValueFields: document.querySelectorAll('.value'),
+   timerCaptionFields: document.querySelectorAll('.label'),
 };
 
+// Timer styling
 refs.timerBox.style.display = 'flex';
-// refs.timerBoxCells.forEach( elem => {elem.style.cssText = 'display: flex; flex-direction: column; justify-content: center;'});
+refs.timerBoxCells.forEach(elem => elem.style.cssText = 'display: flex; flex-direction: column; justify-content: center; margin: 20px;');
+refs.timerValueFields.forEach(elem => elem.style.cssText = 'display: flex; justify-content: center; font-size: 24px; font-weight: 600;');
+refs.timerCaptionFields.forEach(elem => elem.style.cssText = 'font-size: 20px; text-transform: uppercase');
+// font-size: 20px; text-transform: uppercase;
+// Timer styling
 
 
 
-// refs.timerBox.children.style = '"display: flex; flex-direction: column; justify-content: center;"';
-// refs.timerBox.children.style.flex-direction = 'column';
