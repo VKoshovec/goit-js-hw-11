5b5d1046fc55658eca36c8d1f903a373e5ea2d43
VladimirKoshovec <koshavladmir@gmail.com>
2022-11-28 11:28:44 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import throttle from 'lodash.throttle';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    inputCountry: document.querySelector('#search-box'),
+}
+
+refs.inputCountry.addEventListener('input', onInput);
+
+function onInput () {
+    console.log("Auch!");
+}
