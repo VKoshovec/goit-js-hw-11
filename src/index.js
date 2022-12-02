902b6934cec9c31876189b390e6e237121f87b7d
VladimirKoshovec <koshavladmir@gmail.com>
2022-12-02 16:24:51 +0200

    start

--- src/index.js
+++ src/index.js
@@ -2,119 +2,117 @@ import { trim } from 'lodash';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import './css/styles.css';
-import { fetchCountry } from './js/fetchCountries';
+import { fetchImages } from './js/fetchImages';
 
-//vars
-const DEBOUNCE_DELAY = 300;
+// //vars
+// const DEBOUNCE_DELAY = 500;
 
 const refs = {
-    inputCountry: document.querySelector('#search-box'),
-    countryInfo: document.querySelector('.country-info'),
-    countryList: document.querySelector('.country-list'),
-}
-
-//add style
-document.body.style.backgroundColor = '#ffffff';
-const inputStyle = refs.inputCountry.style;
-inputStyle.margin = '30px';
-inputStyle.width = '500px';
-
+    searchForm: document.querySelector('#search-form'),
+};
 
 //events
-refs.inputCountry.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+refs.searchForm.addEventListener('submit', onSubmit);
+
 
 //functions
-function onInput (event) {
-    clearAllInfo ();
+function onSubmit (e) {
+  e.preventDefault();
+  
+}
 
-    const searchName = trim(event.target.value);
+// //functions
+// function onInput (event) {
+//     clearAllInfo ();
 
-    if(trim(searchName)) {
+//     const searchName = trim(event.target.value);
 
-        searcStart();
-        fetchCountry(searchName).then(json => {
+//     if(trim(searchName)) {
 
-            chackJson (json, renderArrJson, renderOneCountryJson);  
-            searchFinish ();
+//         searcStart();
+//         fetchCountry(searchName).then(json => {
+
+//             chackJson (json, renderArrJson, renderOneCountryJson);  
+//             searchFinish ();
               
-        }).catch(err => {
+//         }).catch(err => {
             
-            searchFinish ();
-            Notiflix.Notify.failure(`Oops, there is no country with that name "${searchName}`);
+//             searchFinish ();
+//             Notiflix.Notify.failure(`Oops, there is no country with that name "${searchName}`);
 
-        });  
-    }
-}
+//         });  
+//     }
+// }
 
-function chackJson (testJson, callbackTenToOne, callbackOne) {
+// function chackJson (testJson, callbackTenToOne, callbackOne) {
 
-    if (testJson.length < 10 && testJson.length !==1) {
-        callbackTenToOne(testJson);
-    } else if (testJson.length == 1) {
-        callbackOne(testJson);
-    } else {
-        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-    }
-}
+//     if (testJson.length < 10 && testJson.length !==1) {
+//         callbackTenToOne(testJson);
+//     } else if (testJson.length == 1) {
+//         callbackOne(testJson);
+//     } else {
+//         Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+//     }
+// }
 
-function renderArrJson (jsonArr) {
+// function renderArrJson (jsonArr) {
   
-    const htmlMap = jsonArr.map(element => 
-       `<li style="display: flex; margin: 5px">
-            <img style="width: 30px; height: 30px; margin-right: 10px" src=${element.flags.svg}>
-            <p style="font-size: 20px; margin: 0px">${element.name}</p>
-        </li>`
-    ).join("");
+//     const htmlMap = jsonArr.map(element => 
+//        `<li style="display: flex; margin: 5px">
+//             <img style="width: 30px; height: 30px; margin-right: 10px" src=${element.flags.svg}>
+//             <p style="font-size: 20px; margin: 0px">${element.name}</p>
+//         </li>`
+//     ).join("");
       
-    refs.countryList.insertAdjacentHTML('beforeend', htmlMap);
+//     refs.countryList.insertAdjacentHTML('beforeend', htmlMap);
   
-};
+// };
 
-function renderOneCountryJson (jsonArr) {
+// function renderOneCountryJson (jsonArr) {
   
-    const htmlMap = jsonArr.map(element => {
+//     const htmlMap = jsonArr.map(element => {
 
-       const langs = element.languages.map(lang => lang.name).join(', ');
+//        const langs = element.languages.map(lang => lang.name).join(', ');
 
-       return `<ul><li style="display: flex; align-items: center">
-            <img style="width: 30px; height: 30px; margin-right: 10px" src=${element.flags.svg}>
-            <p style="font-size: 30px; font-weight: bold; margin: 0px">${element.name}</p>
-          </li>
-          <li style="display: flex;">
-            <p style="font-size: 18px; font-weight: bold; margin: 0px 10px 0px 0px; padding:0px;" >Capital: </p>
-            <p style="font-size: 18px; margin:0px 10px 0px 0px; padding:0px;">${element.capital}</p>
-          </li>
-          <li style="display: flex;">
-            <p style="font-size: 18px; font-weight: bold; margin:0px 10px 0px 0px; padding:0px; ">Population: </p>
-            <p style="font-size: 18px; margin:0px; padding:0px 10px 0px 0px;">${element.population}</p>
-          </li>
-          <li style="display: flex;">
-            <p style="font-size: 18px; font-weight: bold; margin:0px 10px 0px 0px; padding:0px; ">Languages: </p>
-            <p style="font-size: 18px; margin:0px; padding:0px 10px 0px 0px;">${ langs }</p>
-          </li></ul>`
-     }    
-    );
+//        return `<ul><li style="display: flex; align-items: center">
+//             <img style="width: 30px; height: 30px; margin-right: 10px" src=${element.flags.svg}>
+//             <p style="font-size: 30px; font-weight: bold; margin: 0px">${element.name}</p>
+//           </li>
+//           <li style="display: flex;">
+//             <p style="font-size: 18px; font-weight: bold; margin: 0px 10px 0px 0px; padding:0px;" >Capital: </p>
+//             <p style="font-size: 18px; margin:0px 10px 0px 0px; padding:0px;">${element.capital}</p>
+//           </li>
+//           <li style="display: flex;">
+//             <p style="font-size: 18px; font-weight: bold; margin:0px 10px 0px 0px; padding:0px; ">Population: </p>
+//             <p style="font-size: 18px; margin:0px; padding:0px 10px 0px 0px;">${element.population}</p>
+//           </li>
+//           <li style="display: flex;">
+//             <p style="font-size: 18px; font-weight: bold; margin:0px 10px 0px 0px; padding:0px; ">Languages: </p>
+//             <p style="font-size: 18px; margin:0px; padding:0px 10px 0px 0px;">${ langs }</p>
+//           </li></ul>`
+//      }    
+//     );
      
-    refs.countryInfo.insertAdjacentHTML('beforeend', htmlMap);
+//     refs.countryInfo.insertAdjacentHTML('beforeend', htmlMap);
   
-    console.log(jsonArr);
-};
+//     console.log(jsonArr);
+// };
 
-function clearAllInfo () {
+// function clearAllInfo () {
 
-    refs.countryInfo.innerHTML = '';
-    refs.countryList.innerHTML = '';
+//     refs.countryInfo.innerHTML = '';
+//     refs.countryList.innerHTML = '';
 
-}
+// }
 
-function searcStart () {
-    Notiflix.Loading.standard("Searching...");
-    // refs.inputCountry.setAttribute ('disabled', true);
-}
+// function searcStart () {
+//     Notiflix.Loading.standard("Searching...");
+//     // refs.inputCountry.setAttribute ('disabled', true);
+// }
 
-function searchFinish () {
-    Notiflix.Loading.remove();
-    // refs.inputCountry.removeAttribute('disabled');
-    refs.inputCountry.focus();
-}
+// function searchFinish () {
+//     Notiflix.Loading.remove();
+//     // refs.inputCountry.removeAttribute('disabled');
+//     refs.inputCountry.focus();
+// }
 
---
+++ src/js/fetchImages.js
@@ -0,0 +1,19 @@
+import Notiflix from "notiflix";
+import { trim } from 'lodash';
+
+
+export function fetchImages (imgName) {
+    
+//     const searchParams = new URLSearchParams({
+//         fields: "name,capital,population,flags,languages",
+//     });
+
+//     const fetchString = `https://restcountries.com/v2/name/${trim(countryName)}?${searchParams}`;
+        
+//     return fetch(fetchString).then(response => {
+//         if (!response.ok) {
+//            throw new Error(response.status) 
+//         } 
+//         return response.json();
+//     });
+}
